4c387a9e60283a07e756ccc4528cf49f7781de06
Fan Yang <fan.yang.oxford@gmail.com>
2019-11-26 09:34:24 +0000

    add new mask format, move to applymask.py

--- applymask.py
+++ applymask.py
@@ -14,6 +14,25 @@ def load_mask_format1(mask_filepath):
     mask = "".join([line.strip() for line in lines[1:]])
     return mask
 
+# dwyllie format
+# one position per line
+def load_mask_format2(mask_filepath, length):
+    with open(mask_filepath) as f:
+        lines = f.readlines()
+    masked_positions = set()
+    for line in lines:
+        try:
+            masked_positions.add(int(line))
+        except ValueError:
+            print(f"masked position '{line}' is not an integer")
+    mask = list()
+    for i in range(0, length):
+        if i in masked_positions:
+            mask.append('1')
+        else:
+            mask.append('0')
+    return mask
+
 def load_fasta(fasta_filepath):
     with open(fasta_filepath) as f:
         lines = f.readlines()
@@ -65,16 +84,24 @@ def apply_mask(mask, sequence):
 def main():
     p = argparse.ArgumentParser()
     p.add_argument("mask_filepath")
+    p.add_argument("mask_format")
     p.add_argument("fasta_filepath")
     p.add_argument("use_gzip")
     args = p.parse_args()
-
-    mask = load_mask_format1(args.mask_filepath)
 
     if args.use_gzip == "true":
         (fasta_header, fasta_sequence, fasta_chunk_len) = load_fasta_gzip(args.fasta_filepath)
     else:
         (fasta_header, fasta_sequence, fasta_chunk_len) = load_fasta(args.fasta_filepath)
+
+    if args.mask_format == "format1":
+        mask = load_mask_format1(args.mask_filepath)
+    elif args.mask_format == "dwyllie":
+        mask = load_mask_format2(args.mask_filepath, len(fasta_sequence))
+    else:
+        print(f"unknown mask format: {args.mask_format}")
+        print("expected one of: format1, dwyllie")
+        return
 
     new_sequence = apply_mask(mask, fasta_sequence)
 
